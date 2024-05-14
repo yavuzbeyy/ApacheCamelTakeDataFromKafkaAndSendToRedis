2c029b95cd57452ba2eabc56ce5939cfe521d42a
Yavuz <devyavuz81@gmail.com>
2024-05-14 11:26:35 +0300

    calisanVaziyette

--- src/main/java/org/apache/camel/learn/MyRouteBuilder.java
+++ src/main/java/org/apache/camel/learn/MyRouteBuilder.java
@@ -15,7 +15,7 @@ public class MyRouteBuilder extends RouteBuilder {
         // here is a sample which processes the input files
         // (leaving them in place - see the 'noop' flag)
         // then performs content based routing on the message using XPath
-        from("file:src/data?noop=true")
+        /*from("file:src/data?noop=true")
             .choice()
                 .when(xpath("/person/city = 'London'"))
                     .log("UK message")
@@ -23,6 +23,25 @@ public class MyRouteBuilder extends RouteBuilder {
                 .otherwise()
                     .log("Other message")
                     .to("file:target/messages/others");
-    }
+					
+		 from("kafka:localhost:59092?topic=192.168.20.104.dbo.UploadImages")
+            .log("Received message from Kafka: ${body}");
+    }*/
+	
+	from("kafka:192.168.20.104.dbo.UploadImages?brokers=localhost:59092")
+    .process(exchange -> {
+        // Kafka'dan gelen mesajı işle
+        String message = exchange.getIn().getBody(String.class);
+        // İşlenen mesajı logla
+        System.out.println("Received message from Kafka: " + message);
+        // Mesajı işleme, veritabanına yazma, vb. işlemleri burada gerçekleştir
+    });
 
+	
+	/*public void configure() {
+       from("direct:start")
+    .to("http://localhost:5062/api/Author/ListAll")
+    .log("Received response from API: ${body}");*/
+
+    }
 }
